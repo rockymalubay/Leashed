fe79a0a37cf6e24a65c74f857ed22faced676351
rockymalubay <rockymalubay@gmail.com>
2020-10-22 10:54:29 -0700

    React-native template

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Button from './Comps/Button';
+
+const App = () => {
+  return <View style={styles.container}>
+
+  
+    <Button />
+
+
+  </View>
+
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red", 
+    justifyContent:"center",
+    alignItems: "center",
+  }
+
+})
+export default App;
---
+++ Comps/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const MyButton = () => {
+    //no more divs
+
+    return <View>
+        <Text>Button to Signup</Text>
+        <Button 
+            title="Signup"
+            color="#FADBAD"
+            
+        />
+    </View>
+
+}
+
+export default MyButton; 
